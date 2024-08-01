792806d3365a4527568a10acc30e82f4b3fb78d2
haikal-266 <haikalrizky266@gmail.com>
2024-08-01 19:50:09 +0700

    create model and controller

---
+++ app/Http/Controllers/ProducTransactionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProducTransaction;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProducTransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProducTransaction $producTransaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProducTransaction $producTransaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProducTransaction $producTransaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProducTransaction $producTransaction)
+    {
+        //
+    }
+}
